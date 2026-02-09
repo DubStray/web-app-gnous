358c83f6839ae86ea118239cc1b9f294f8acb309
DubStray <francesco.gaming00@gmail.com>
2026-02-09 18:19:07 +0100

    feat: added ENUMs for task status and task priority

---
+++ technicaltest-backend/src/main/java/com/francesco/technicaltest_backend/entity/enums/TaskPriority.java
@@ -0,0 +1,12 @@
+package com.francesco.technicaltest_backend.entity.enums;
+
+/**
+ * Enum che rappresenta la priorità di un task.
+ * 
+ * @author Francesco
+ */
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ technicaltest-backend/src/main/java/com/francesco/technicaltest_backend/entity/enums/TaskStatus.java
@@ -0,0 +1,12 @@
+package com.francesco.technicaltest_backend.entity.enums;
+
+/**
+ * Enum che rappresenta lo stato di un task
+ * 
+ * @author Francesco
+ */
+public enum TaskStatus {
+    TODO,
+    DOING,
+    DONE
+}

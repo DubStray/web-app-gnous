e88591d3d35f837b38a050252f4dde93e600897b
DubStray <francesco.gaming00@gmail.com>
2026-02-09 16:48:30 +0100

    Initial Commit: setup backend and frontend

---
+++ technicaltest-backend/src/main/java/com/francesco/technicaltest_backend/TechnicaltestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.francesco.technicaltest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicaltestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicaltestBackendApplication.class, args);
+	}
+
+}
---
+++ technicaltest-backend/src/test/java/com/francesco/technicaltest_backend/TechnicaltestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francesco.technicaltest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicaltestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

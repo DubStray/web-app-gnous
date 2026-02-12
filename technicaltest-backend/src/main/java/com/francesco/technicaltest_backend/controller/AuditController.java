c9b3fd6a1f5a4318b0c36ca585c1a6a3a49f54ec
DubStray <francesco.gaming00@gmail.com>
2026-02-12 15:28:11 +0100

    feat: add Audit API to filter logs by type

--- technicaltest-backend/src/main/java/com/francesco/technicaltest_backend/controller/AuditController.java
+++ technicaltest-backend/src/main/java/com/francesco/technicaltest_backend/controller/AuditController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.francesco.technicaltest_backend.dtos.AuditLogDTO;
+import com.francesco.technicaltest_backend.entity.enums.AuditLogEventType;
 import com.francesco.technicaltest_backend.service.AuditLogService;
 /**
  * Controller per la gestione dei log di audit.
@@ -25,5 +27,11 @@ public class AuditController {
     @GetMapping("/logs")
     public ResponseEntity<List<AuditLogDTO>> getLogs() {
         return ResponseEntity.ok(this.auditLogService.getAllLogs());
+    }
+
+    // Filtro i log per tipo
+    @GetMapping("/logs/filter")
+    public ResponseEntity<List<AuditLogDTO>> getLogsByType(@RequestParam AuditLogEventType eventType) {
+        return ResponseEntity.ok(this.auditLogService.getLogsByType(eventType));
     }
 }
